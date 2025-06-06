f334a5e5c7682ae39c691e1a0ab165adb3c4b4f1
ErwanDelc <edelcroix@lyreco.com>
2025-06-06 15:26:59 +0200

    POC RPA HelloWorld bot

---
+++ test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldRPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from RPA bot!");
+        }
+    }
+}
